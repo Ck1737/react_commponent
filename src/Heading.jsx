f5c65e95cb903525d91f54b0ebbd30fa6203b644
Ck1737 <savaliyachirag930@gmail.com>
2023-07-17 11:24:54 +0530

    component

---
+++ src/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+  return <h1>My Name Is Vinod</h1>;
+}
+
+export default Heading;
---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <ol>
+      <li>Ahemdabad</li>
+      <li>Surat</li>
+      <li>Gandhinagar</li>
+      <li>Amreli</li>
+      <li>Vadodra</li>
+    </ol>
+  );
+}
+
+export default List;
